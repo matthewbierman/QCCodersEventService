16cc7e01d1dab2b3f4cdd74f822bb9c060c93449
Michael Liendo <model3volution@gmail.com>
2017-10-24 02:02:40 -0500

    chore: add frozen object to separate concerns

---
+++ data/endpoints.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+  testICalURL: "https://calendar.google.com/calendar/ical/matthewbierman.com_qmsnneuuh1k6btlqli2ina82s4%40group.calendar.google.com/public/basic.ics",
+  testICalLocalPath: "./data/testCalendar.ics",
+  productionICalURL: "https://calendar.google.com/calendar/ical/qccoders%40gmail.com/public/basic.ics"
+})
