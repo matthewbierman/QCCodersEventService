631524928939b26f6d4faef364e1eb695b0f9772
Glitch (qccoderseventservice) <none>
2018-04-09 22:11:24 +0000

    changed endpoint/query for meetups

--- data/endpoints.js
+++ data/endpoints.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
   testICalURL: "https://calendar.google.com/calendar/ical/matthewbierman.com_qmsnneuuh1k6btlqli2ina82s4%40group.calendar.google.com/public/basic.ics",
   testICalLocalPath: "./testCalendar.ics",
   productionICalURL: "https://calendar.google.com/calendar/ical/qccoders%40gmail.com/public/basic.ics",
-  meetupsAPIFetchEventsURL: "https://api.meetup.com/QCCoders/events?&scroll=future_or_past&page=20&desc=true"
+  meetupsAPIFetchEventsURL: "https://api.meetup.com/qccoders/events?scroll=recent_past&page=20"
 })
